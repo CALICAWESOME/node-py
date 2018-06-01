5854a714a5da8929727d1c291fc61cf5c08a8bab
Bobby Martin <greendolphinavenue@gmail.com>
2018-06-01 13:26:34 -0400

    countdown to disconnect instead of a one-off message

--- client.js
+++ client.js
@@ -1,13 +1,26 @@
+#!/usr/bin/env node
 const net = require('net');
+
+const msgDelay = 1000;	// delay in between sending messages
+let interval;
+let count = 10;
 
 const client = net.createConnection({ port: 8080 }, () => {
 	console.log('JS: connected!');
-	client.write('ECHO ME PLS');
+	interval = setInterval(() => {
+		if (!count) {
+			clearInterval(interval);
+			client.write('DIE');
+			return;
+		}
+		console.log(`JS: sending: ${count}`);
+		client.write(count-- + '');
+	}, msgDelay);
 });
 client.on('data', data => {
-	console.log(`JS: response: ${data.toString()}`);
-	client.end();
+	console.log(`JS: received: ${data.toString()}`);
 });
 client.on('end', () => {
 	console.log('JS: disconnected!');
 });
+
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import socket
 
 TCP_IP = 'localhost'
@@ -12,9 +13,8 @@ conn, addr = s.accept()
 print('PYTHON: listening at {}:{}'.format(TCP_IP, TCP_PORT))
 while 1:
 	data = conn.recv(BUFFER_SIZE)
-	if not data: break
-	print('PYTHON: recieved: ', data)
+	if data.decode().rstrip('\n') == 'DIE' or not data: break
+	print('PYTHON: received: ', data)
 	print('PYTHON: sending: ', data)
 	conn.send(data) # ECHO!!!
-conn.close()
 
